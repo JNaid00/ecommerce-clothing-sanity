064c220701c0774c7d989e7df0d8136c62f923cb
JesseUbuntu <jessenaidoo24@gmail.com>
2023-01-18 23:01:17 +0200

    Fixed add to cart button

--- components/ProductItem.jsx
+++ components/ProductItem.jsx
@@ -18,7 +18,7 @@ import { addToCart, setItems } from "@/store/cartSlice";
 import { shades } from "@/styles/theme";
 const ProductItem = ({ item, width }) => {
   const dispatch = useDispatch();
-  const [count, setcount] = useState(0);
+  const [count, setcount] = useState(1);
   const [isHovered, setisHovered] = useState(false);
   const { image, price, name, category, _id } = item;
   let SARand = new Intl.NumberFormat("en-ZA", {
@@ -46,8 +46,8 @@ const ProductItem = ({ item, width }) => {
 
         <div
           className={`${
-            isHovered ? "blocked" : "hidden"
-          } absolute bottom-[10%] left-0 w-full px-[5%]`}
+            isHovered ? "lg:blocked" : "lg:hidden"
+          } absolute bottom-[10%] left-0 w-full px-[5%] blocked`}
         >
           <div className="flex justify-between">
             <div className="flex items-center bg-[#f5f5f5] rounded-sm space-x-1 ">
